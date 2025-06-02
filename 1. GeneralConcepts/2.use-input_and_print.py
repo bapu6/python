eb4c4cce8ccfa4eedf02c2aa25700e65c9d1c40e
Bapu <bapu.pradhan20@gmail.com>
2025-06-02 19:23:53 +0000

    python basics

---
+++ 1. GeneralConcepts/2.use-input_and_print.py
@@ -0,0 +1,16 @@
+input() # This code will wait for user input without any prompt message
+
+input("Enter something: ") # This code will wait for user input with a prompt message
+
+print("My name is Bapu") # This code will print the user's input after they enter something
+print("Hello, World!")  # This code will print "Hello, World!" to the console
+
+print(f"Hello, {input('Enter your name: ')}!")  # This code will print a greeting with the user's name
+
+x=2
+y=3
+z=4
+print(x,y,z) # This code will print the values of x, y, and z separated by spaces
+print(x,y,z, sep='-')  # This code will print the values of x, y, and z separated by hyphens
+print(x,y,z, sep='|')  # This code will print the values of x, y, and z separated by vertical bars
+print(x,y,z, sep=' ')  # This code will print the values of x, y, and z separated by spaces
