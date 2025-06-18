b10345434befb7571ef28565964a1a56d42c3919
Bapu <bapu.pradhan20@gmail.com>
2025-06-18 07:04:10 +0000

    testing changes

---
+++ 2. DataTypes/2.numerical.py
@@ -0,0 +1,30 @@
+# numerical
+
+# integer variable
+x=5
+print("type of x: ", type(x))
+
+# float variable
+price=3.14
+num = 3e6
+print("type of price: ", type(price))
+print("type of num: ", type(num))
+
+# complex variable
+z=2+3j
+print("type of z: ", type(z))
+
+
+# soe built-in functions for numerical types
+print("abs(-5): ", abs(-5))  # Absolute value
+print("round(3.14159, 2): ", round(3.14159, 2))  # Rounding to 2 decimal places
+print("pow(2, 3): ", pow(2, 3))  # Power function (2 raised to the power of 3)
+# Converting between numerical types
+print("int(3.14): ", int(3.14))  # Convert float to int
+print("float(5): ", float(5))  # Convert int to float
+
+# Getting the minimum and maximum of a list of numbers
+numbers = [1, 2, 3, 4, 5]
+print("min(numbers): ", min(numbers))  # Minimum value in the list
+print("max(numbers): ", max(numbers))  # Maximum value in the list
+
